8bbc61001c295b1c053ac88070d12f5d4dc25f27
Santhosh Babu M R <santhosh.infotec@gmail.com>
2019-09-01 14:56:23 +0530

    u

---
+++ src/main/java/enums/EnumExample.java
@@ -0,0 +1,30 @@
+package enums;
+
+enum INDIAN_STATES {
+    ANDHRA_PRADESH("AP"),
+    TELENGANA("TE"),
+    TAMILNADU("TN"),
+    KERALA("KE"),
+    KARNATAKA("KA"),
+    MADHYA_PRADESH("MP");
+
+    private String code;
+    INDIAN_STATES(String code) {
+        this.code = code;
+    }
+    @Override
+    public String toString() {
+        return this.code;
+    }
+}
+
+public class EnumExample {
+    public static void main(String[] args) {
+        INDIAN_STATES[] indian_states = INDIAN_STATES.values();
+        for(INDIAN_STATES state: indian_states) {
+            String out = String.format("%d: state: %s, state.toString(): %s", state.ordinal()+1, state, state.toString());
+            System.out.println(out);
+        }
+        System.out.println(INDIAN_STATES.KERALA.equals(INDIAN_STATES.valueOf("KERALA")));
+    }
+}
